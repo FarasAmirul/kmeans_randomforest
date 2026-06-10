1860605ff2e962ec2a4606032a0d3c897ebf9f3b
Faras Amirul <farasarray@gmail.com>
2026-06-10 16:53:52 +0700

    prepare ml service deploy

--- ml-service/app.py
+++ ml-service/app.py
@@ -64,5 +64,8 @@ def random_forest():
     return jsonify(df.to_dict(orient='records'))
 
 # ================== RUN ==================
+import os
+
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
